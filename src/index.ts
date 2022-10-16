15e43974a40c6f40e59ec01455769816673825b0
Miguel Ángel Barquilla <46623652+Mibarfu@users.noreply.github.com>
2022-10-16 12:49:49 +0200

    Implementa el PokemonCardPreview module

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import PokeApi from "./components/PokeApi/PokeApi.js";
 import type PokeData from "./components/PokeApi/types.js";
+import PokemonCardPreview from "./components/PokemonCardPreview/PokemonCardPreview.js";
 
 const apiUrl = "https://pokeapi.co/api/v2";
 const apiEndpoint = "pokemon";
 
+const parentElement = document.querySelector(".root");
+
 const pokemon = new PokeApi(apiUrl, apiEndpoint);
 
 const pokemonResponse = (await pokemon.request(1)) as PokeData;
+
+const card = new PokemonCardPreview(
+  parentElement as HTMLElement,
+  1,
+  "Pepe",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png"
+);
+card.render();
